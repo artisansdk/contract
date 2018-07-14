86cab1ce2c5cbb4eee35ac84b8d56efc99f3c3b7
Daniel LaBarge <daniel@bexarcreative.com>
2018-07-14 09:33:49 -0500

    Fix Arrayable.

--- src/Event.php
+++ src/Event.php
@@ -2,9 +2,10 @@
 
 namespace ArtisanSdk\Contract;
 
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Support\Jsonable;
 use JsonSerializable;
 
-interface Event extends Arryable, Jsonable, JsonSerializable
+interface Event extends Arrayable, Jsonable, JsonSerializable
 {
 }
