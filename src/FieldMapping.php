67b996a52e9a66006fd3a57dc571248372db0518
Daniel LaBarge <daniel@bexarcreative.com>
2019-07-31 14:42:25 -0500

    Add a new contract for field mapping.

---
+++ src/FieldMapping.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ArtisanSdk\Support\Contract;
+
+interface FieldMapping
+{
+    /**
+     * Get the field mappings.
+     *
+     * @return array
+     */
+    public function mappings(): array;
+}
